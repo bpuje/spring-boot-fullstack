da5a7d22235992605a056608e1eab00846af6c5c
bpuje <swpuujee@gmail.com>
2023-06-05 23:57:46 -0500

    added new line

--- backend/src/main/java/com/amigoscode/Main.java
+++ backend/src/main/java/com/amigoscode/Main.java
@@ -7,21 +7,19 @@ import com.github.javafaker.Name;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-
     }
 
     @Bean
     CommandLineRunner runner(CustomerRepository customerRepository) {
+
         return args -> {
             var faker = new Faker();
             Random random = new Random();
