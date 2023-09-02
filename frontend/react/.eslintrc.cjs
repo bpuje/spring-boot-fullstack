fe70f094671eefe954e8519ed6b88bd07586f24b
bpuje <swpuujee@gmail.com>
2023-09-02 12:37:45 -0500

    updated customer drawer and forms

--- frontend/react/.eslintrc.cjs
+++ frontend/react/.eslintrc.cjs
@@ -1,20 +1,15 @@
-/* eslint-env node */
-
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "latest" } }, // Updated to use the latest version
+  plugins: ["react", "react-hooks"], // Added 'react-hooks'
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    "react/prop-types": "off", // You can turn off prop-types rule if you prefer
   },
-}
+};
