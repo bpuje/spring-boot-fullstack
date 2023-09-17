66991670f4bb8b17efdb220443618c6e9839269f
bpuje <swpuujee@gmail.com>
2023-09-16 20:55:45 -0500

    Login - React check2

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -37,7 +37,7 @@ const App = () => {
                 <Spinner
                     thickness='4px'
                     speed='0.65s'
-                    emptyColor='gray.220'
+                    emptyColor='gray.200'
                     color='blue.500'
                     size='xl'
                 />
